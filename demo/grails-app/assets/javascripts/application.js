a8903a58c1c5085c11993b9ab96a5dee96703c8d
Jeff Scott Brown <brownj@ociweb.com>
2016-08-08 18:51:59 -0500

    Use Grails 3.1.10

--- demo/grails-app/assets/javascripts/application.js
+++ demo/grails-app/assets/javascripts/application.js
@@ -2,10 +2,11 @@
 //
 // Any JavaScript file within this directory can be referenced here using a relative path.
 //
-// You're free to add application-wide JavaScript to this file, but it's generally better 
+// You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-2.1.3.js
+//= require jquery-2.2.0.min
+//= require bootstrap
 //= require_tree .
 //= require_self
 
